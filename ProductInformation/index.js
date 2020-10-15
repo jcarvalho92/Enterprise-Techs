d2de0c9e56f119d22433210007d9bacc3aeeaaae
Juliana <julianadecarvalho92@gmail.com>
2020-10-15 13:54:56 -0400

    added request(GET, POST, DELETE); Validated inputs

--- ProductInformation/index.js
+++ ProductInformation/index.js
@@ -1,14 +1,73 @@
-fs = require('fs') 
-fs.readFile('package.json', 'utf8', function(err,data) {
-    if (err) { 
-        return console.log(err); 
-    } 
-    console.log(data); 
+
+//Joi class to define a schema (minimum/maximum of characteres etc)
+const Joi = require('joi'); //Pascal name convention for classes
+
+const express = require('express');
+const app = express();
+
+//adding a piece of midleware 
+app.use(express.json());
+
+//testing using an array
+const products = [
+    {id: 1, productName: 'a', price:  100, category: "test"},
+    {id: 2, productName: 'b', price:  200, category: "test"},
+    {id: 3, productName: 'c', price:  300, category: "test"},
+]
+
+//defining the route
+//GET REQUESTS 
+app.get('/api/products', (req, res) => {
+    res.send(products);
 });
 
-var http = require('http');
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("product information");
-} );
-server.listen(8000, '127.0.0.1')
+app.get('/api/products/:id', (req, res) =>{
+    const product = products.find(c => c.id === parseInt(req.params.id));
+
+    if(!product) 
+        res.status(404).send('The product with the given ID was not found');
+    res.send(product);
+});
+
+//POST REQUESTS
+app.post('/api/products', (req,res) =>{
+    //validanting the input
+    const schema = Joi.object({ productName: Joi.string() .min(3) .required(),
+        price: Joi.number() .required(),
+        category: Joi.string() .min(2) .required() });
+        
+    const result = schema.validate(req.body);
+    
+    if (result.error){
+        //400 bad request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const product = {
+        id: products.length +1,
+        productName: req.body.productName,
+        price: req.body.price,
+        category: req.body.category
+    };
+    products.push(product);
+    //return in the body
+    res.send(product);
+})
+
+//DELETE REQUESTS
+app.delete('/api/products/:id',(req,res) => {
+    const product = products.find(c => c.id === parseInt(req.params.id));
+
+    if(!product) 
+        res.status(404).send('The product with the given ID was not found');
+    
+
+    const index = products.indexOf(product);
+    products.splice(index,1);
+    res.send(product);
+});
+
+const port = 3000
+app.listen(port,() => console.log(`Listening on port ${port}...`)); //Log the information on start-up
+
