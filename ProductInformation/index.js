82198e6d8c0a544c25ec65b385be73526782e971
Juliana <julianadecarvalho92@gmail.com>
2020-10-14 21:46:00 -0400

    added logging errors

--- ProductInformation/index.js
+++ ProductInformation/index.js
@@ -1,3 +1,11 @@
+fs = require('fs') 
+fs.readFile('package.json', 'utf8', function(err,data) {
+    if (err) { 
+        return console.log(err); 
+    } 
+    console.log(data); 
+});
+
 var http = require('http');
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
